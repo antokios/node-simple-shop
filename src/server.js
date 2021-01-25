dca9761677d812c5c752032fa0e60977e8719dd1
unknown <antokios@gmail.com>
2021-01-25 16:10:23 +0200

    Changed server PORT to 4000

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ const compression = require('compression')
 const app = express();
 
 // define port to run express app
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 // use bodyParser middleware on express app
 app.use(bodyParser.urlencoded({ extended: true }));
